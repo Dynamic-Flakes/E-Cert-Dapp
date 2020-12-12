d2c4830104582f51a172b27c6e46fee2ab86a190
melitus <asmelitus@gmail.com>
2020-12-12 10:11:17 +0100

    refactor: make changes on the verify hash function

--- contracts/CertificateRegistry.sol
+++ contracts/CertificateRegistry.sol
@@ -74,12 +74,14 @@ contract CertificateRegistry is Ownable {
         emit LogNewHashStored(msg.sender, block.timestamp, true);
     }
 
+
     function verifyHash(bytes32 _documenteHash) public view returns (bool) {
-        bool valid = isHashStored(_documenteHash);
-        if (valid) {
+        if (documentRegistry[_documenteHash].isStored) {
             return true;
         }
+        return false;
     }
+
 
     function isHashStored(bytes32 _documentHash) internal view returns (bool) {
         return documentRegistry[_documentHash].isStored == true;
